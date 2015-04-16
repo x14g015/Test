4e47fbe56e55a24fac4c43a45a6e3d4f63dcb573
x14g015 <x14g015@PC505N013.chiba-fjb.ac.jp>
2015-04-16 15:01:17 +0900

    テスト

---
+++ src/Test1.java
@@ -0,0 +1,15 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+	
+		
+		int[] a = {5,8,2,1,4};
+		int i;
+		for(i=0;i<a.length;i++)
+		{
+		    System.out.println(a[i]);
+		}
+	}
+
+}
---
+++ src/test.java
@@ -0,0 +1,35 @@
+public class test {
+
+	public static void main(String[] args) {
+		// int a = 10;
+		// float b = 100.5f;
+		// double c = 1000.15;
+		// char d = 'M';
+		// String e = "ふぉ";
+		//
+		// String s =
+		// String.format("整数:%5d\n小数:%f\n小数:%f\n文字:%c\n文字列:%s\n",a,b,c,d,e);
+		// System.out.println(s);
+
+		// String s = "AbCdE";
+		// //小文字
+		// System.out.println(s.toLowerCase());
+		// //大文字
+		// System.out.println(s.toUpperCase());
+
+		// String s = "ABCDEFABCDEF";
+		// //CDをXYに置換
+		// String s2 = s.replaceAll("CD", "XY");
+		// System.out.println(s2);
+
+		// System.out.print("こんにちは世界");
+		// System.err.print("こんにちは世界2");
+
+		int i;
+		int a = 0;
+		// iを0に初期設定;i<3を満たしていたら処理を実行;処理終了後、iに1を足す
+		for (i = 0; i < 3; i++)
+			a = a + i;
+	}
+
+}
